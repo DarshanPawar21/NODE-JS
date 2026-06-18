d5dabc21d6c32a596282cd7f237f5eae85619944
DARSHAN PAWAR <dp7878038@gmail.com>
2026-06-18 17:48:18 +0530

    ...

--- DAY-5/server.js
+++ DAY-5/server.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-
+import { ObjectId } from "mongodb";
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 const dbname = "student-db";
@@ -30,10 +30,19 @@ const getstudent = async () => {
 const removestudent = async () => {
     const db = await connnectDB();
     await db.collection("student").deleteOne({
-        name :"Darshan Pawar",
+        _id: new ObjectId("6a33e16aea74ad97baad687a")
     });
     console.log("students data delete successfuly !");
 }
+
+const updatestudent = async () => {
+    const db = await connnectDB();
+    const result = await db.collection("student").updateOne(
+        { _id: new ObjectId("6a33e1cadba0621da0da491a") },
+        { $set: { course: "Full stck devlopment" } });
+    console.log("Data update successfuly !");
+}
 // AddStudent();
 // getstudent();
-removestudent();
+// removestudent();
+updatestudent();
