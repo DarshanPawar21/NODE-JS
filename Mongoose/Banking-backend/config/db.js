d7c152140c33336d47bd2a99697f9b720e2fd2bc
DARSHAN PAWAR <dp7878038@gmail.com>
2026-08-15 09:59:35 +0530

    Delete Mongoose/Banking-backend directory

--- Mongoose/Banking-backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () =>{
-    mongoose.connect("mongodb://localhost:27017/banking");
-    console.log("mongo connect successfuly !");
-}
-module.exports = connectDB;
